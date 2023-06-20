d73e3002d5b4ecb6574ac298892122f2c30edcff
song-junho <saa106@shinyoung.com>
2023-06-20 10:13:27 +0900

    FEAT: 코드 구조 수정

--- main.py
+++ main.py
@@ -1,10 +1,8 @@
-from asset import stock
+from theme import theme_index
 
 start_date = "2006-01-01"
-end_date   = "2023-06-01"
+end_date   = "2023-06-30"
 
 if __name__ == "__main__":
 
-    # stock.Value(start_date, end_date).create_factor_data()
-    stock.Growth(start_date, end_date).create_factor_data()
-    # stock.Size(start_date, end_date).create_factor_data()
+    theme_index.ThemeIndex(start_date, end_date).create_theme_index()
